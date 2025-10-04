7980242d79f391179a9c8e42094e56ba3495fb8a
Raven <jraven.tabag@gmail.com>
2025-10-04 21:50:17 +0800

    Added .NET skill to Software Dev, Added Others Category for Trello, Figma, and Agile SDLC ; Joined Git at Tools Category during webview

--- src/modules/pages/home/content/_data/skills.js
+++ src/modules/pages/home/content/_data/skills.js
@@ -1,10 +1,14 @@
 import { ICO_AndroidSTD } from '../icons/ICO_AndroidSTD';
 import { ICO_CPP } from '../icons/ICO_CPP';
+import { ICO_CS } from '../icons/ICO_CS';
 import { ICO_CSharp } from '../icons/ICO_CSharp';
 import { ICO_CSS3 } from '../icons/ICO_CSS3';
 import { ICO_DevAndroid } from '../icons/ICO_DevAndroid';
+import { ICO_DevMore } from '../icons/ICO_DevMore';
 import { ICO_DevSoftware } from '../icons/ICO_DevSoftware';
 import { ICO_DevWebsite } from '../icons/ICO_DevWebsite';
+import { ICO_DotNet } from '../icons/ICO_DotNet';
+import { ICO_Figma } from '../icons/ICO_Figma';
 import { ICO_FireStore } from '../icons/ICO_FireStore';
 import { ICO_Git } from '../icons/ICO_Git';
 import { ICO_HTML5 } from '../icons/ICO_HTML5';
@@ -14,8 +18,10 @@ import { ICO_NodeJS } from '../icons/ICO_NodeJS';
 import { ICO_QT } from '../icons/ICO_QT';
 import { ICO_ReactJS } from '../icons/ICO_ReactJS';
 import { ICO_Realm } from '../icons/ICO_Realm';
+import { ICO_SDLC } from '../icons/ICO_SDLC';
 import { ICO_SQLite } from '../icons/ICO_SQLite';
 import { ICO_Tailwind } from '../icons/ICO_Tailwind';
+import { ICO_Trello } from '../icons/ICO_Trello';
 import { ICO_VS } from '../icons/ICO_VS';
 import { ICO_VSCode } from '../icons/ICO_VSCode';
 
@@ -23,7 +29,6 @@ const skillset = [
   {
     Icon: ICO_DevWebsite,
     title: 'Web Development',
-    description: 'Building modern, responsive, and scalable web apps.',
     skills: [
       { Icon: ICO_HTML5, label: 'HTML5' },
       { Icon: ICO_CSS3, label: 'CSS3' },
@@ -45,7 +50,7 @@ const skillset = [
       },
 
       {
-        label: 'Frameworks and Libraries',
+        label: 'Libraries & Frameworks',
         items: [
           { Icon: ICO_Tailwind, label: 'Tailwind' },
           { Icon: ICO_ReactJS, label: 'ReactJS' },
@@ -54,13 +59,11 @@ const skillset = [
       },
 
       {
-        label: 'Version Control',
-        items: [{ Icon: ICO_Git, label: 'Git' }],
-      },
-
-      {
-        label: 'Tools/IDEs',
-        items: [{ Icon: ICO_VSCode, label: 'VSC' }],
+        label: 'Tools',
+        items: [
+          { Icon: ICO_Git, label: 'Git' },
+          { Icon: ICO_VSCode, label: 'VSC' }
+        ],
       },
     ],
   },
@@ -68,7 +71,6 @@ const skillset = [
   {
     Icon: ICO_DevAndroid,
     title: 'Android Development',
-    description: 'Creating native and cross-platform mobile applications.',
     skills: [
       { Icon: ICO_Java, label: 'Java' },
       { Icon: ICO_Realm, label: 'Realm' },
@@ -93,13 +95,10 @@ const skillset = [
       },
 
       {
-        label: 'Version Control',
-        items: [{ Icon: ICO_Git, label: 'Git' }],
-      },
-
-      {
-        label: 'Tools/IDEs',
-        items: [{ Icon: ICO_AndroidSTD, label: 'AS' }],
+        label: 'Tools',
+        items: [
+          { Icon: ICO_Git, label: 'Git' },
+          { Icon: ICO_AndroidSTD, label: 'AS' }],
       },
     ],
   },
@@ -107,10 +106,10 @@ const skillset = [
   {
     Icon: ICO_DevSoftware,
     title: 'Software Development',
-    description: 'Designing and engineering software solutions.',
     skills: [
       { Icon: ICO_CPP, label: 'C++' },
       { Icon: ICO_CSharp, label: 'C#' },
+      { Icon: ICO_DotNet, label: '.NET' },
       { Icon: ICO_QT, label: 'Qt' },
       { Icon: ICO_VS, label: 'VS' }
     ],
@@ -120,15 +119,42 @@ const skillset = [
         items: [
           { Icon: ICO_CPP, label: 'C++' },
           { Icon: ICO_CSharp, label: 'C#' }
-
         ],
       },
 
+            {
+        label: 'Libraries & Frameworks',
+        items: [
+          { Icon: ICO_DotNet, label: '.NET' },
+        ],
+      },
+
+
       {
-        label: 'Tools/IDEs',
+        label: 'Tools',
         items: [
           { Icon: ICO_QT, label: 'Qt' },
           { Icon: ICO_VS, label: 'VS' }
+        ],
+      },
+    ],
+  },
+
+  {
+    Icon: ICO_DevMore,
+    title: 'Others',
+    skills: [
+          { Icon: ICO_Trello, label: 'Trello' },
+          { Icon: ICO_Figma, label: 'Figma' },
+          { Icon: ICO_SDLC, label: 'Agile' },
+    ],
+    skillscat: [
+      {
+        label: 'Others  ',
+        items: [
+          { Icon: ICO_Trello, label: 'Trello' },
+          { Icon: ICO_Figma, label: 'Figma' },
+          { Icon: ICO_SDLC, label: 'Agile' },
         ],
       },
     ],
